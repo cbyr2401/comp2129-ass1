d6c78ed175f2e6e877d94f1495ea327267973be3
Cian Byrne <Cian Byrne>
2016-03-20 22:20:04 +1100

    cleaned up function layouts.

--- minesweeper.c
+++ minesweeper.c
@@ -13,35 +13,118 @@ int maxRows;
 int maxCols;
 
 // function declarations:
+void initializeArr(char *p);
+void plantMine(char *p, int row, int col);
+void printGridDebug(char const *p);
+void CleanExitError(void);
 
 
-int printGrid(char const *p, int rows, int cols){
-	printf("PRINTGRID: ptr: %p | x: %d | y: %d\n", p, rows, cols);
-	for(int i=0; i<rows; i++){
-		for(int j=0; j<cols; j++){
-			printf("%c", p[i * cols+j]);
+int checkGrid(){
+	return 0;
+}
+
+int main(int argc, char **argv){
+	int x,y;
+	char cmd;
+	
+	// first command MUST be 'g':
+	fflush(stdin);
+	if (scanf("%c %d %d", &cmd, &x, &y) == 1 
+		|| cmd != 'g' 
+		|| (x < 0 && x > 100) 
+		|| (y < 0 && y > 100)
+		|| (x * y < 10)
+		)
+	{
+		// catch error conditions...
+		CleanExitError();
+	}
+	// declare array (x:cols:width, y:rows:height):
+	char field[y * x];
+	
+	// declare GLOBAL constants for 
+	//  maximum number of Rows and Columns:
+	maxRows = y;
+	maxCols = x;
+	
+	// initialize array:
+	initializeArr(field);
+	
+	// DEBUG: print array:
+	//printGridDebug(field);
+	
+	// print command:
+	printf("%c %d %d\n", cmd, x, y);
+
+	// second is 10 times 'b':
+	int count = 0;
+	
+	while (count < 10){
+		fflush(stdin);
+		scanf("%c %d %d", &cmd, &x, &y);
+		if(cmd=='b' && x>-1 && y>-1 && x<maxCols && y<maxRows){
+			// updated grid (row major)
+			plantMine(field,y,x);
+			
+			// print command:
+			printf("%c %d %d\n", cmd, x, y);
+			
+			// increment count:
+			count++;
+		}else{
+			CleanExitError();
+		}
+	}
+
+	// after 10 successful 'b', print grid:
+	//printGrid();
+
+	// GAME ON!!  Keep going until an exit condition is met.
+	while(1){
+		scanf("%c %d %d", &cmd, &x, &y);
+
+		if(cmd=='u'){
+			//updateGrid();
+			checkGrid();
+			printf("%c %d %d\n", cmd, x, y);
+			//printGrid(pfield);
+		}
+		else if(cmd=='f'){
+			//updateGrid();
+			checkGrid();
+			printf("%c %d %d\n", cmd, x, y);
+			//printGrid(pfield);
+		}
+		else{
+			printf("error");
+		}
+	}
+
+	return 0;
+}
+
+// function used to check that mines are in correct location.
+void printGridDebug(char const *p){
+	for(int i=0; i<maxRows; i++){
+		for(int j=0; j<maxCols; j++){
+			printf("%c", p[i * maxCols+j]);
 		}
 		printf("\n");
 	}
 	fflush(stdout);
-	return 0;
 }
 
-int initializeArr(char *p, int rows, int cols){
-	
-	for(int i=0; i<rows; i++){
-		for(int j=0; j<cols; j++){
-			p[i * cols+j] = 48;
+// function used to fill array with '0'
+void initializeArr(char *p){
+	for(int i=0; i<maxRows; i++){
+		for(int j=0; j<maxCols; j++){
+			p[i * maxCols+j] = 48;
 		}
 	}
-	return 0;
 }
 
-int updateGridPlant(char *ptrGrid,int row, int col, int maxRows, int maxCols){
-	//  GRID
-	//  TL  TC  TR
-	//  L    *   R
-	//  BL  BC  BR
+// function to plant a mine with a few safety checks.
+void plantMine(char *ptrGrid,int row, int col){
 	/*
 		Safety checks in place:
 			1.  Check if mine already place
@@ -105,95 +188,11 @@ int updateGridPlant(char *ptrGrid,int row, int col, int maxRows, int maxCols){
 		}
 	}
 	ptrGrid[row*maxCols+col] = MINE;
-	return 0;
 }
 
-int CleanExitError(void){
+
+// function to exit program.
+void CleanExitError(void){
 	printf("error");
 	exit(0);
-	return 0;
-}
-
-int checkGrid(){
-	return 0;
-}
-
-int main(int argc, char **argv){
-	int x,y;
-	char cmd;
-	
-	// first command MUST be 'g':
-	fflush(stdin);
-	if (scanf("%c %d %d", &cmd, &x, &y) == 1 
-		|| cmd != 'g' 
-		|| (x < 0 && x > 100) 
-		|| (y < 0 && y > 100)
-		|| (x * y < 10)
-		)
-	{
-		// catch error conditions...
-		CleanExitError();
-	}
-	// declare array (x:cols:width, y:rows:height):
-	char field[y * x];
-	
-	// declare some constants for width and height:
-	maxRows = y;
-	maxCols = x;
-	
-	// initialize array:
-	initializeArr(field,maxRows,maxCols);
-	
-	// DEBUG: print array:
-	printGrid(field,maxRows,maxCols);
-	
-	// print command:
-	printf("%c %d %d\n", cmd, x, y);
-
-	// second is 10 times 'b':
-	int count = 0;
-	
-	while (count < 10){
-		fflush(stdin);
-		scanf("%c %d %d", &cmd, &x, &y);
-		if(cmd=='b' && x>-1 && y>-1 && x<maxCols && y<maxRows){
-			// updated grid (row major)
-			updateGridPlant(field,y,x,maxRows,maxCols);
-			
-			// print command:
-			printf("%c %d %d\n", cmd, x, y);
-			
-			// DEBUG: print array:	
-			printGrid(field,maxRows,maxCols);
-			count++;
-		}else{
-			CleanExitError();
-		}
-	}
-
-	// after 10 successful 'b', print grid:
-	//printGrid();
-
-	// GAME ON!!  Keep going until an exit condition is met.
-	while(1){
-		scanf("%c %d %d", &cmd, &x, &y);
-
-		if(cmd=='u'){
-			//updateGrid();
-			checkGrid();
-			printf("%c %d %d\n", cmd, x, y);
-			//printGrid(pfield);
-		}
-		else if(cmd=='f'){
-			//updateGrid();
-			checkGrid();
-			printf("%c %d %d\n", cmd, x, y);
-			//printGrid(pfield);
-		}
-		else{
-			printf("error");
-		}
-	}
-
-	return 0;
 }
